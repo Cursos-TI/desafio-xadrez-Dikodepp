a74c199399868650177aa227cb322ca19c303890
Dikodepp <dikodepp@gmail.com>
2025-04-07 09:04:40 +0000

    implementação do cavalo (nivel aventureiro)

--- xadrez.c
+++ xadrez.c
@@ -4,12 +4,18 @@ int main(){
     
     int bispo = 0;
     int rainha = 0;
+    int movimentoDocavalo = 1;
+
+    printf("Torre:\n");
 
     for (int i = 0; i < 5; i++) // movimento da torre 5 casas para a direita
     {
         printf("Direita\n");
     }
 
+    printf("\n");
+
+    printf("Bispo:\n");
     //movimento do bispo 5 casa para cima, direita
     do
     {
@@ -17,11 +23,29 @@ int main(){
         bispo ++;
     } while (bispo < 5);
     
+    printf("\n");
+
+    printf("Rainha:\n");
     //movimento da rainha 8 casas para esquerda
     while (rainha < 8)
     {
         printf("Esquerda \n");
         rainha ++;
+    }
+
+    printf("\n");
+
+    printf("Cavalo:\n");
+    // movimento do cavalo 2 vezes para cima e uma pra direita
+
+    while (movimentoDocavalo--)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            printf("Baixo\n");
+        }
+        printf("Esquerda\n");
+        
     }
     
 
